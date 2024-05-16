6a20fd467abc1bd5b30a4585e5eb174b6451eea4
GuilhermeGuimaraesDeMedeiros <gg.medeiros2011@gmail.com>
2024-05-16 15:07:19 -0300

    Form Validation

--- Task7/script.js
+++ Task7/script.js
@@ -1,7 +1,20 @@
-const submitBtn = document.getElementById("submitBtn")
-function check() {
-    
-    alert("Almost there, but we have no server yet!")
-}
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  'use strict'
 
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
 
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
